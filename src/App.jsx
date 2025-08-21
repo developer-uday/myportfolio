e2424191b6b529373c8a1008ea8bab58ea1ad667
Uday Pratap Singh <developeruday6202@gmail.com>
2025-08-21 21:49:55 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from './components/Home';
 import Navbar from "./components/Navbar";
 import "./index.css";
-import { Routes, Route } from "react-router-dom";
-import Home from './components/Home';
-import Projects from './components/Projects';
-import ConatctMe from './components/Contact-me';
-import Cursor from "./components/Cursor";
 
 const App = () => {
   return (
@@ -14,8 +10,6 @@ const App = () => {
       {/* <Cursor /> */}
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/projects" element={<Projects />}/>
-        <Route path="/contact-us" element={<ConatctMe />}/>
       </Routes>
     </>
   );
